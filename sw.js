d4eced9aa73911a06f98fbdda80eeaf87aca9adc
raceon0987 <114948086+raceon0987@users.noreply.github.com>
2026-06-30 13:34:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,23 @@
 const CACHE_NAME = "savings-app-v1";
-const urlsToCache = [
-  "/savings-tracker/",
-  "/savings-tracker/index.html",
-  "/savings-tracker/manifest.json"
-];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "/savings-tracker/",
+        "/savings-tracker/index.html",
+        "/savings-tracker/manifest.json"
+      ])
+    )
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match("/savings-tracker/index.html");
+      });
+    })
   );
 });
